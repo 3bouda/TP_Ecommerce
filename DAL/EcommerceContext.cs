57d6822ed478d9f49f833770a799c6e51cdbb892
3bouda <abdelaliiheb@gmail.com>
2024-07-08 02:48:03 +0100

    Add project files.

---
+++ DAL/EcommerceContext.cs
@@ -0,0 +1,21 @@
+﻿using TP_Ecommerce.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+namespace TP_Ecommerce.DAL
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext() : base("EcommerceContext")
+        {
+        }
+        public DbSet<Categorie> Categories { get; set; }
+        public DbSet<Marque> Marques { get; set; }
+        public DbSet<Produit> Produits { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
